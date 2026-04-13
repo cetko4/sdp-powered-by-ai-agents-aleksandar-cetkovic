27f52b30da75b5db7248a99113ceb3d78f8e8801
Aleksandar <aleksandarcetkovic4@gmail.com>
2026-04-13 19:41:09 +0200

    #5 feat(c4-context): add system context diagram

--- scripts/generate-plantuml.sh
+++ scripts/generate-plantuml.sh
@@ -3,7 +3,7 @@ set -e
 
 for file in $(git diff --cached --name-only --diff-filter=ACM | grep '\.puml$' || true); do
   echo "Generating SVG for $file"
-  plantuml -tsvg "$file"
+  java -Dplantuml.allowincludeurl=true -jar "$HOME/.local/share/plantuml/plantuml.jar" -tsvg "$file"
   svg_file="${file%.puml}.svg"
   git add "$svg_file"
 done
